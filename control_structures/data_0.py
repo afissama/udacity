34b36a6ab7b568ef75a99a7bf074f82a13de7eb7
Afis Malick <afis.kousse@strategyobject.com>
2024-01-31 17:48:39 +0000

    :sparkles: iterate control dict

--- control_structures/data_0.py
+++ control_structures/data_0.py
@@ -5,6 +5,20 @@ def smallest_positive(in_list):
             smallest = item
     return smallest
 
+#Suggested solution
+def smallest_positive_solution(in_list):
+    # TODO: Define a control structure that finds the smallest positive
+    # number in in_list and returns the correct smallest number.
+    smallest_pos = None
+    for num in in_list:
+        if num > 0:
+            # Note: we use a logical "or" in this solution to form 
+            # the conditional statement, although this was
+            # not introduced above. 
+            if smallest_pos == None or num < smallest_pos:
+                smallest_pos = num
+    return smallest_pos
+
 # Test cases
 
 print(smallest_positive([4, -6, 7, 2, -4, 10]))
