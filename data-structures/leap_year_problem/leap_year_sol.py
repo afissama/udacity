bf9b252853bc586fd811b12497d53775bb2ac340
Afis Malick <afis.kousse@strategyobject.com>
2024-02-06 17:56:26 +0000

    exo: days between dates

--- data-structures/leap_year_problem/leap_year_sol.py
+++ data-structures/leap_year_problem/leap_year_sol.py
@@ -42,3 +42,30 @@ def nextDay(year, month, day):
             return year, month+1,1
         else:
             return year+1, 1, 1
+
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    """Returns the number of days between year1/month1/day1
+       and year2/month2/day2. Assumes inputs are valid dates
+       in Gregorian calendar, and the first date is not after
+       the second."""
+    year, month, day = year1, month1, day1
+    nb = 0
+    while (year, month, day) != (year2, month2, day2):
+        (year, month, day) = nextDay(year, month, day)
+        nb +=1
+    # YOUR CODE HERE!
+    return nb
+
+def test():
+    test_cases = [((2012,9,30,2012,10,30),30), 
+                  ((2012,1,1,2013,1,1),360),
+                  ((2012,9,1,2012,9,4),3)]
+    
+    for (args, answer) in test_cases:
+        result = daysBetweenDates(*args)
+        if result != answer:
+            print ("Test with data:", args, "failed")
+        else:
+            print ("Test case passed!")
+
+test()
