b5580f47c546c6a66cc266dac948c7e5da27294d
Afis Malick <afis.kousse@strategyobject.com>
2024-02-09 16:09:35 +0000

    ans: task_0

--- data-structures/unscramble-sc-problem/P0/P0/Task0.py
+++ data-structures/unscramble-sc-problem/P0/P0/Task0.py
@@ -6,10 +6,12 @@ import csv
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
+    print("First record of texts, {} texts {}  at time {}".format(texts[0][0], texts[0][1], texts[0][2]))
 
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
+    print("Last record of calls, {} calls {} at time {}, lasting {} seconds, ".format(calls[len(calls) -1][0], calls[len(calls) -1][1], calls[len(calls) -1][2], calls[len(calls) -1][3]))
 
 
 """
